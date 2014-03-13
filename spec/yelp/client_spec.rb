7c38630a04da5a319e72f59f364d657f63ea41ea
Tomer Elmalem <telmalem@gmail.com>
2014-03-13 11:27:49 -0700

    Refactor client spec to prevent reused code

--- spec/yelp/client_spec.rb
+++ spec/yelp/client_spec.rb
@@ -1,16 +1,19 @@
 require 'yelp'
 
 describe Yelp::Client do
+  let(:keys) { Hash[consumer_key: 'abc',
+                    consumer_secret: 'def',
+                    token: 'ghi',
+                    token_secret: 'jkl'] }
+
+  before do
+    @client = Yelp::Client.new(keys)
+  end
+
   describe 'client initialization' do
     it 'should create a client with the appropriate keys set' do
-      keys = { consumer_key: 'abc',
-               consumer_secret: 'def',
-               token: 'ghi',
-               token_secret: 'jkl' }
-
-      client = Yelp::Client.new(keys)
       Yelp::Client::AUTH_KEYS.each do |key|
-        client.send(key).should eql keys[key]
+        @client.send(key).should eql keys[key]
       end
     end
   end
