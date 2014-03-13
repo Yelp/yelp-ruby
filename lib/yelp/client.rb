5e6edbb811f481b4de0627cb45c343354fdb1b9c
Tomer Elmalem <telmalem@gmail.com>
2014-03-13 11:36:49 -0700

    Setup oauth for making requests to the api

--- lib/yelp/client.rb
+++ lib/yelp/client.rb
@@ -1,13 +1,21 @@
+require 'oauth'
+
 module Yelp
   class Client
     AUTH_KEYS = [:consumer_key, :consumer_secret, :token, :token_secret]
+    API_HOST  = 'http://api.yelp.com'
 
-    attr_reader *AUTH_KEYS
+    attr_reader *AUTH_KEYS, :access_token
 
     def initialize(options = {})
       AUTH_KEYS.each do |key|
         instance_variable_set("@#{key}", options[key])
       end
+    end
+
+    def authorize
+      consumer      ||= OAuth::Consumer.new(@consumer_key, @consumer_secret, site: API_HOST)
+      @access_token ||= OAuth::AccessToken.new(consumer, @token, @token_secret)
     end
   end
 end
--- spec/yelp/client_spec.rb
+++ spec/yelp/client_spec.rb
@@ -17,4 +17,19 @@ describe Yelp::Client do
       end
     end
   end
+
+  describe 'oauth authorization' do
+    it 'should successfully create an oauth client with the appropriate values set' do
+      @client.authorize
+
+      @client.access_token.class.should eql OAuth::AccessToken
+
+      @client.access_token.consumer.options[:site].should eql Yelp::Client::API_HOST
+      @client.access_token.consumer.key.should eql keys[:consumer_key]
+      @client.access_token.consumer.secret.should eql keys[:consumer_secret]
+
+      @client.access_token.secret.should eql keys[:token_secret]
+      @client.access_token.token.should eql keys[:token]
+    end
+  end
 end
