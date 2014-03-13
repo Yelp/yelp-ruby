dff259855244022536b0a9e18864f0555567c59d
Tomer Elmalem <telmalem@gmail.com>
2014-03-13 10:40:44 -0700

    Clean up old code to test testing

--- lib/yelp.rb
+++ lib/yelp.rb
@@ -2,8 +2,5 @@ require "yelp/version"
 
 module Yelp
   class << self
-    def client
-      true
-    end
   end
 end
--- spec/yelp_spec.rb
+++ spec/yelp_spec.rb
@@ -1,7 +1,4 @@
 require 'yelp'
 
 describe Yelp do
-  it "should return true" do
-    Yelp.client.should eql true
-  end
 end
