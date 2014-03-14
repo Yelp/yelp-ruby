5ddca0749af2200c7136070b67bcad3f98058419
Tomer Elmalem <telmalem@gmail.com>
2014-03-14 10:00:21 -0700

    Reorganize search methods and comment the bounding box search

--- lib/yelp/client/search.rb
+++ lib/yelp/client/search.rb
@@ -17,18 +17,21 @@ module Yelp
         DeepStruct.new(JSON.parse(search_request(params).body))
       end
 
-      # Make a request against the search endpoint from the API
-      # and return the raw response
-      def search_request(params)
-        @connection.get PATH, params
-      end
-
+      # Search by a bounding box: specify a south west lat/long and a ne lat/long
+      # along with regular parameters to make a request to the search endpoint
+      # More info at: http://www.yelp.com/developers/documentation/v2/search_api#searchGBB
       def search_by_bounding_box(sw_latitude, sw_longitude, ne_latitude, ne_longitude, params = {}, locale = {})
         options = { bounds: "#{sw_latitude},#{sw_longitude}|#{ne_latitude},#{ne_longitude}" }
         options.merge!(params)
         options.merge!(locale)
 
         DeepStruct.new(JSON.parse(search_request(options).body))
+      end
+
+      # Make a request against the search endpoint from the API
+      # and return the raw response
+      def search_request(params)
+        @connection.get PATH, params
       end
     end
   end
