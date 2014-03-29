ed48547b78561ae33d07d75e5374d37120cdc5cb
Tomer Elmalem <telmalem@gmail.com>
2014-03-29 12:47:13 -0700

    Set the default error messages in each error class

--- lib/yelp/error.rb
+++ lib/yelp/error.rb
@@ -1,6 +1,21 @@
 module Yelp
   class Error < StandardError; end
-  class MissingAPIKeys < Error; end
-  class MissingLatLng < Error; end
-  class BoundingBoxNotComplete < Error; end
+
+  class MissingAPIKeys < Error
+    def initialize(msg = "You're missing an API key")
+      super
+    end
+  end
+
+  class MissingLatLng < Error
+    def initialize(msg = 'Missing required latitude or longitude parameters')
+      super
+    end
+  end
+
+  class BoundingBoxNotComplete < Error
+    def initialize(msg = 'Missing required values for bounding box')
+      super
+    end
+  end
 end
