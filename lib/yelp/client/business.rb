e6a5fc757d9b572932fe477662b11a90671e5fb4
Tomer Elmalem <telmalem@gmail.com>
2014-03-29 12:38:05 -0700

    Add yard docs to a methods

--- lib/yelp/client/business.rb
+++ lib/yelp/client/business.rb
@@ -5,8 +5,15 @@ module Yelp
     module Business
       PATH = '/v2/business/'
 
-      # Return a formatted/structured response from a request
-      # to the business endpoint at the API
+      # Make a request to the business endpoint on the API
+      #
+      # @param id [String] the business id
+      # @return [DeepStruct] the parsed response object from the API
+      #
+      # @example Get business
+      #   business = client.business('yelp-san-francisco')
+      #   business.name # => 'Yelp'
+      #   buinesss.url  # => 'http://www.yelp.com/biz/yelp-san-francisco'
       def business(id)
         DeepStruct.new(JSON.parse(business_request(id).body))
       end
@@ -14,6 +21,9 @@ module Yelp
       # Make a request to the business endpoint of the API
       # The endpoint requires a format of /v2/business/{business-id}
       # so the primary request parameter is concatenated
+      #
+      # @param id [String, Integer] the business id
+      # @return [Faraday::Response] the raw response back from the connection
       def business_request(id)
         @connection.get PATH + id
       end
