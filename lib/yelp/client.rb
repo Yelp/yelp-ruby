c0eec474cd4d6081d76e0b0fc676295115d6ff00
Tomer Elmalem <telmalem@gmail.com>
2014-04-17 16:37:37 -0700

    Add comments on the methods that define the singleton_methods on client

--- lib/yelp/client.rb
+++ lib/yelp/client.rb
@@ -54,6 +54,9 @@ module Yelp
     end
     alias_method :configure_connection, :configure_faraday
 
+    # This goes through each endpoint class and creates singletone methods
+    # on the client that query those classes. We do this to avoid possible
+    # namespace collisions in the future when adding new classes
     def define_request_methods
       REQUEST_CLASSES.each do |request_class|
         endpoint_instance = request_class.new(self)
@@ -61,12 +64,16 @@ module Yelp
       end
     end
 
+    # Loop through all of the endpoint instances' public singleton methods to
+    # add the method to client
     def create_methods_from_instance(instance)
       instance.public_methods(false).each do |method_name|
         add_method(instance, method_name)
       end
     end
 
+    # Define the method on the client and send it to the endpoint instance
+    # with the args passed in
     def add_method(instance, method_name)
       define_singleton_method(method_name) do |*args|
         instance.public_send(method_name, *args)
