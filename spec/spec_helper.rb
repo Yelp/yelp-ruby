a4e01e52afff776c0181415d5aa8416bac94e105
Tomer Elmalem <telmalem@gmail.com>
2014-04-28 09:54:11 -0700

    Add fixtures for VCR

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,4 +6,9 @@ require 'vcr'
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   c.hook_into :webmock
+
+  c.filter_sensitive_data('<YELP_CONSUMER_KEY>') { ENV['YELP_CONSUMER_KEY'] }
+  c.filter_sensitive_data('<YELP_CONSUMER_SECRET>') { ENV['YELP_CONSUMER_SECRET'] }
+  c.filter_sensitive_data('<YELP_TOKEN>') { ENV['YELP_TOKEN'] }
+  c.filter_sensitive_data('<YELP_TOKEN_SECRET>') { ENV['YELP_TOKEN_SECRET'] }
 end
