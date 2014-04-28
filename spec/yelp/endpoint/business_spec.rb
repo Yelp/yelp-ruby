ddbb744ec6058d7c0238ffd1d0f85d23dd3f0f0d
Tomer Elmalem <telmalem@gmail.com>
2014-04-27 17:00:24 -0700

    Refactor the error integration test into a shared example

--- spec/yelp/endpoint/business_spec.rb
+++ spec/yelp/endpoint/business_spec.rb
@@ -19,14 +19,8 @@ describe Yelp::Endpoint::Business do
   end
 
   describe 'errors' do
-    context 'API' do
-      let(:response_body) { "{\"error\": {\"text\": \"error message\", \"id\": \"INTERNAL_ERROR\"}}" }
-      let(:bad_response)  { double('response', status: 400, body: response_body) }
-
-      it 'should raise an error' do
-        client.stub_chain(:connection, :get).and_return(bad_response)
-        expect { client.business(business) }.to raise_error(Yelp::InternalError)
-      end
+    it_behaves_like 'a request error' do
+      let(:request) { client.business(business) }
     end
   end
 end
