02305f2bb28c34e0a35d70dcdf219fea9ea2184f
Tomer Elmalem <telmalem@gmail.com>
2015-10-22 17:24:01 -0700

    Parse phone search response

--- lib/yelp/responses/phone_search.rb
+++ lib/yelp/responses/phone_search.rb
@@ -1,6 +1,18 @@
+require 'yelp/responses/base'
+require 'yelp/responses/models/business'
+require 'yelp/responses/models/region'
+
 module Yelp
   module Response
-    class PhoneSearch
+    class PhoneSearch < Base
+      attr_accessor :businesses, :region, :total
+
+      def initialize(json)
+        super(json)
+
+        @businesses = parse(@businesses, Model::Business)
+        @region     = parse(@region, Model::Region)
+      end
     end
   end
 end
