bf5dc6e980a0fce97928de8208d671e6293f4c21
Carl Shimer <carl@openbay.com>
2016-07-01 10:55:56 -0400

    fixes an issue where the message property on StandardError wasn't available due to a missing super call.  Also expose the field property for InvalidParameter to enable better error handling.

--- lib/yelp/error.rb
+++ lib/yelp/error.rb
@@ -47,13 +47,14 @@ module Yelp
 
     class Base < StandardError
       def initialize(msg,error=nil)
+        super(msg)
       end
     end
 
     class AlreadyConfigured < Base
       def initialize(msg = 'Gem cannot be reconfigured.  Initialize a new ' +
           'instance of Yelp::Client.', error=nil)
-        super
+      super
       end
     end
 
@@ -76,9 +77,15 @@ module Yelp
     end
 
     class InvalidParameter < Base
+      attr_reader :text, :field 
+
       def initialize(msg='One or more parameters were invalid', error=nil)
-        msg = msg + ': ' + error['field'] unless error.nil?
-        super
+        unless error.nil?
+          @text = error['text']
+          @field = error['field']
+          msg = msg + ': ' + @field
+        end
+        super(msg,error)
       end
     end
 
