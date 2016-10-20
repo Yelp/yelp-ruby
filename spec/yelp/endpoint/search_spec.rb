8162f76b716f5f7eb076d558bd0263cdcb06ab66
Andrew Mason <andrew@fixedpoint.xyz>
2016-10-20 11:05:00 -0700

    Moves error tests to the blocks of methods they are testing

--- spec/yelp/endpoint/search_spec.rb
+++ spec/yelp/endpoint/search_spec.rb
@@ -41,6 +41,12 @@ describe Yelp::Endpoint::Search do
     it 'should get results' do
       expect(results.businesses.size).to be > 0
     end
+
+    context 'when params are empty' do
+      it 'raises when #search_by_bounding_box params are empty' do
+        expect { client.search_by_bounding_box({}, params) }.to raise_error(Yelp::Error::BoundingBoxNotComplete)
+      end
+    end
   end
 
   describe '#search_by_coordinates' do
@@ -55,22 +61,17 @@ describe Yelp::Endpoint::Search do
     it 'should get results' do
       expect(results.businesses.size).to be > 0
     end
+
+    context 'when params are empty' do
+      it 'raises when #search_by_coordinates params are empty' do
+        expect { client.search_by_coordinates({}, params) }.to raise_error(Yelp::Error::MissingLatLng)
+      end
+    end
   end
 
   describe 'errors' do
     it_behaves_like 'a request error' do
       let(:request) { client.search(location) }
     end
-
-    context 'search' do
-      it 'raises when #search_by_coordinates params are empty' do
-        expect { client.search_by_coordinates({}, params) }.to raise_error(Yelp::Error::MissingLatLng)
-      end
-
-      it 'raises when #search_by_bounding_box params are empty' do
-        expect { client.search_by_bounding_box({}, params) }.to raise_error(Yelp::Error::BoundingBoxNotComplete)
-      end
-    end
-
   end
 end
